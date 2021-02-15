57ce87eb626f659437684d4aaa31360c599a9716
Irene <irenemteixeira@ua.pt>
2021-02-15 17:43:44 +0000

    wip

--- src/pages/budget/budget.service.tsx
+++ src/pages/budget/budget.service.tsx
@@ -1,33 +1,29 @@
 import axios from "axios";
 
 export interface IOrcamentoData {
-  
-   id_planeamento?: number,
-   id_utilizador: number,
-   categoria: number,
-   sub_categoria: number,
-   montante_limite: number,
-   prazo: string,
+  id_planeamento?: number;
+  id_utilizador: number;
+  categoria: number;
+  sub_categoria: number;
+  montante_limite: number;
+  prazo: string;
+}
 
-  }
+const authToken = JSON.parse(localStorage.getItem("token") || "{}");
 
-  const authToken = JSON.parse(localStorage.getItem("token") || "{}");
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Token ${authToken.auth_token} `,
+};
 
+export const SERVICE = {
+  routes: {
+    categorias: "categorias",
+    subcategorias: "sub_categorias",
+    planeamentos: "planeamentos",
+  },
 
-  const headers = { 
-    "Content-Type": "application/json",
-    Authorization: `Token ${authToken.auth_token} `,
- };
-
-  export const SERVICE = {
-    routes: {
-      categorias: "categorias",
-      subcategorias: "sub_categorias",
-      planeamentos: "planeamentos",
-    },
-
-    methods: {
-
+  methods: {
     getCategorias: () => {
       return axios.get(`http://127.0.0.1:8000/${SERVICE.routes.categorias}/`);
     },
@@ -38,19 +34,23 @@ export interface IOrcamentoData {
       );
     },
 
-      createBudget: (data: IOrcamentoData) => {
-        return axios.post(
-          `http://127.0.0.1:8000/${SERVICE.routes.planeamentos}/`,
-          data,
-          {
-            headers,
-          }
-        );
-      },
-
-      getPlaneamentos: () => {
-        return axios.get(`http://127.0.0.1:8000/${SERVICE.routes.planeamentos}/`);
-      },
+    createBudget: (data: IOrcamentoData) => {
+      return axios.post(
+        `http://127.0.0.1:8000/${SERVICE.routes.planeamentos}/`,
+        data,
+        {
+          headers,
+        }
+      );
     },
-  };
-  
+
+    getPlaneamentos: () => {
+      return axios.get(
+        `http://127.0.0.1:8000/${SERVICE.routes.planeamentos}/`,
+        {
+          headers,
+        }
+      );
+    },
+  },
+};
