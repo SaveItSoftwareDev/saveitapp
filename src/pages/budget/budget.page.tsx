ad75b5a55f9a1a2ac90c4f5d8abfc2624175106a
programtiago <62779154+programtiago@users.noreply.github.com>
2021-02-16 21:29:53 +0000

    acerto gráfico do planeamento

--- src/pages/budget/budget.page.tsx
+++ src/pages/budget/budget.page.tsx
@@ -121,9 +121,9 @@ import useFetchPlaneamentos from "./fetchers/useFetchPlaneamentos.hook";
                         {planeamentos.map((plan) => {
                           return (
                             <tr>
-                              <td>{plan.montante_limite}</td>
-                              <td>{plan.id_planeamento}</td>
                               <td>{plan.categoria}</td>
+                              <td>{plan.montante_limite}</td>
+                              <td>{plan.prazo}</td>
                             </tr>
                           );
                         })}
